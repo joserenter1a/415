48dc33baea8ec24a6bc21c797cd373446072efac
Jose Renteria <120423283+joserenter1a@users.noreply.github.com>
2023-06-01 00:51:48 -0700

    P3

--- Projects/P3/dtsv3.c
+++ Projects/P3/dtsv3.c
@@ -6,50 +6,49 @@ This is my own work, apart from examples taken from the handout.
 
 
 */
-
+#include "bxp/bxp.h"
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
+#include <string.h>
 #include <assert.h>
+#include <unistd.h>
 #include <pthread.h>
 #include <sys/time.h>
 #include <valgrind/valgrind.h>
-#include "bxp/bxp.h"
 #include "ADTs/heapprioqueue.h"
 #include "ADTs/queue.h"
 #include "ADTs/hashmap.h"
-// Macros
+
 #define UNUSED __attribute__((unused))
 #define PORT 19999
 #define SERVICE "DTS"
-#define USECS (10 * 1000)
 
-// init the shared data structures and set to NULL
-const PrioQueue *PQ = NULL;
-const Map *map = NULL;
+#define USECS (10 * 1000)
 
 unsigned long global_id = 1;
 pthread_mutex_t lock;
+// Shared Data structures
+const PrioQueue *PQ = NULL;
+const Map *map = NULL;
 
-// define a struct to hold our event data
+
 typedef struct event 
 {
     int oneShot;
     int repeat;
     int cancel;
-    unsigned long num_repeats;
+    unsigned long n_repeats;
     unsigned long interval;
     unsigned long id;
-    unsigned long clid; 
+    unsigned long clid;
     unsigned int port;
-    char *host;
+    char *host; 
     char *service;
 } Event;
 
-// from CADS 12.3
-long int hashFxn(void *key, long int N)
-{
-    return (((unsigned long)key)%N);
+long int hash_fxn(void *key, long int N)
+{ 
+    return (( (unsigned long) key) %N);
 }
 
 /* compare two timeval structs
@@ -104,7 +103,8 @@ int compare_map(void *id1, void *id2)
 }
 
 // This function was given in lab_7/cbserver.c
-int extractWords(char *buf, char *sep, char *words[]) {
+int extractWords(char *buf, char *sep, char *words[]) 
+{
     int i;
     char *p;
 
@@ -123,14 +123,17 @@ unsigned long insert_oneshot(char **words)
     sscanf(words[2], "%ld", &(t->tv_sec));
     sscanf(words[3], "%ld", &(t->tv_sec));
     
+    // init struct variables
     event->id = global_id;
     event->oneShot = 1;
     event->repeat = 0;
     event->cancel = 0;
-    event->num_repeats = 0;
+    event->n_repeats = 0;
     event->interval = 0;
+    // get clid and port
     sscanf(words[1], "%lu", &(event->clid));
     sscanf(words[6], "%u", &(event->port));
+    // duplicate to event struct vars
     event->host = strdup(words[4]);
     event->service = strdup(words[5]);
 
@@ -155,8 +158,10 @@ unsigned long insert_repeat(char **words)
     long s = 0;
     long ms = 0;
     long us = 0;
+    // get ms
     sscanf(words[2], "%ld", &ms);
     us = ms * 1000;
+
     while(us > 1000000)
     {
         us -= 1000000;
@@ -164,19 +169,21 @@ unsigned long insert_repeat(char **words)
     }
 
 
-    // initialize timeval struct
+    // init timeval struct
     t->tv_sec = s;
     t->tv_usec = us;
 
-    // initialize event struct
+    // init event struct
     event->id = global_id;
     event->oneShot = 0;
     event->repeat = 1;
     event->cancel = 0;
-    sscanf(words[3], "%lu", &(event->num_repeats));
+    // get n_repeats, interval, clid, and port
+    sscanf(words[3], "%lu", &(event->n_repeats));
     sscanf(words[2], "%lu", &(event->interval));
     sscanf(words[1], "%lu", &(event->clid));
     sscanf(words[6], "%u", &(event->port));
+    
     event->host = strdup(words[4]);
     event->service = strdup(words[5]);
 
@@ -197,247 +204,218 @@ unsigned long cancel_update(char **words)
     Event *event_ptr;
     unsigned long current = 0;
     unsigned long ret = 0;
-    // get id of event to be cancelled
 
+    // get id of event to be cancelled
     sscanf(words[1], "%lu", &(current));
+
     // get event from map
-    if(map->get(map, (void *)current, (void **)&event_ptr))
+    if(map->get(map, (void *) current, (void **) &event_ptr))
     {
         event_ptr->cancel = 1;
     }
+    // set return value to the global id
     ret = global_id;
+    // increment global id
     global_id++;
 
     return ret;
 }
 
-// Built on top of dtsv2
 void *dtsv3()
 {
-    // Initialize BXP Endpoint and Service
-    BXPEndpoint send;
-    BXPService bxp_service;
+    
+    BXPEndpoint sender;
+    BXPService bxps;
 
-    char *server_query = (char *)malloc(BUFSIZ);
-    char *server_response = (char *)malloc(BUFSIZ + 1);
-    char *query_copy = (char *)malloc(sizeof(server_query));
-    char *command, *parsed;
+    char *query = (char *)malloc(BUFSIZ);
+    char *resp = (char *)malloc(BUFSIZ);
 
-    char *w[25];
+    unsigned len;
+    char *service;
+    unsigned short port;
+
+    char *words[25];
     int N;
-
-    unsigned query_length, response_length;
     unsigned long svid;
 
-    // Initialize our BXP protocol, bound to our local Port
-    assert(bxp_init(PORT, 1));
-
-    // Offer our BXP Service
-    bxp_service = bxp_offer(SERVICE);
-
-    // If the service is null, exit and free
-    if(bxp_service == NULL)
+    service = SERVICE;
+    port = PORT;
+    
+    //Initialize BXP system - bind to ‘port’ if non-zero
+    assert(bxp_init(port, 1));
+    
+    //Offer service named `service' in this process
+    bxps = bxp_offer(service);
+    
+    if (bxps == NULL) 
     {
-        fprintf(stderr, "Failure to initiailize service\n");
-        free(server_response);
-        free(server_query);
+        fprintf(stderr, "Failure offering Echo service\n");
+        free(query);
+        free(resp);
         exit(EXIT_FAILURE);
     }
-
-    while((query_length = bxp_query(bxp_service, &send, server_query, BUFSIZ)) > 0)
+    //obtain the next query message from `bxps' - blocks until message available
+    while ((len = bxp_query(bxps, &sender, query, BUFSIZ)) > 0) 
     {
-        // parse our query string
-        server_query[query_length] = '\0';
-        // copy the query
-        strcpy(query_copy, server_query);
-        command = query_copy;
-
-        N = extractWords(command, "|", w);
-
-        VALGRIND_MONITOR_COMMAND("leak_check summary");
-
-        if(strcmp(w[0], "OneShot") == 0)
+        char cmd[1000];
+        query[len] = '\0';
+        strcpy(cmd, query);
+        N = extractWords(cmd, "|", words);
+        if((strcmp(words[0], "OneShot") == 0) && N == 7)
         {
-            switch(N)
-            {
-                case 7:
-                    svid = insert_oneshot(w);
-                    sprintf(server_response, "1%s", server_query);
-                    fprintf(stdout, "%s - Server Request Success\n", server_response);
-                    break;
-                default:
-                    sprintf(server_response, "0%s", server_query);
-            }
-            VALGRIND_MONITOR_COMMAND("leak_check summary");        
+                svid = insert_oneshot(words);
+                VALGRIND_MONITOR_COMMAND("leak_check summary");
         }
-        else if(strcmp(w[0], "Repeat") == 0)
+        else if((strcmp(words[0], "Repeat") == 0) && N == 7)
         {
-            switch(N)
-            {
-                case 7:
-                    svid = insert_repeat(w);
-                    sprintf(server_response, "1%s", server_query);
-                    fprintf(stdout, "%s - Server Request Success\n", server_response);
-                    break;
-                default:
-                    sprintf(server_response, "0%s", server_query);
-            }
-            VALGRIND_MONITOR_COMMAND("leak_check summary");
+                svid = insert_repeat(words);
+                VALGRIND_MONITOR_COMMAND("leak_check summary");
         }
-        
-        else if(strcmp(w[0], "Cancel") == 0)
+        else if((strcmp(words[0], "Cancel") == 0) && N == 2)
         {
-            switch(N)
-            {
-                case 2:
-                    svid = cancel_update(w);
-                    sprintf(server_response, "1%s", server_query);
-                    fprintf(stdout, "%s - Server Request Success\n", server_response);
-                    break;
-                default:
-                    sprintf(server_response, "0%s", server_query);
-            }
-            VALGRIND_MONITOR_COMMAND("leak_check summary");
+                svid = cancel_update(words);
+                VALGRIND_MONITOR_COMMAND("leak_check summary");
         }
         else
         {
-            svid = 0;
+            svid = 0;  
         }
+
         if(svid == 0)
-        {
-            strcpy(server_response, "0");
-        }
-        else 
-        {
-            sprintf(server_response, "1%08lu", svid);
-        }
+            strcpy(resp, "0");
+        else
+            sprintf(resp, "1%08lu", svid);
+        
+        bxp_response(bxps, &sender, resp, strlen(resp) + 1);
+    }
+    free(query);
+    free(resp);
 
-        VALGRIND_MONITOR_COMMAND("leak_check summary");
-        response_length = strlen(server_response) + 1;
-        bxp_response(bxp_service, &send, server_response, response_length);
-    }   
-
-    free(server_query);
-    free(server_response);
     pthread_exit(NULL);
 }
 
-void *timer(UNUSED void *args)
+void *timer(UNUSED void *args) 
 {
     struct timeval now;
     struct timeval *later = (struct timeval *)malloc(sizeof(struct timeval));
-
-    while(1)
+    
+    while(1) 
     {
+        
         usleep(USECS);
         gettimeofday(&now, NULL);
 
-        //Queue to store events that are ready to harvest
-        const Queue *ready_q = Queue_create(NULL);
-
-        // Queue to store repeat events
-        const Queue *repeat_q = Queue_create(NULL);
+        //Queue to store ready to harvest events
+        const Queue *queue = Queue_create(NULL);
+        //Queue to store the repeat events
+        const Queue *r_queue = Queue_create(NULL);
 
         struct timeval *tval_ptr;
 
-        Event *event_ptr;
+        Event *eve_ptr;
 
-        // Get events that are ready to harvest, add to ready_q
+        /* Get the events that are ready to harvest,
+           Then add to queue */
         pthread_mutex_lock(&lock);
-        while(PQ->min(PQ, (void **)&tval_ptr, (void **)&event_ptr))
+        while(PQ->min(PQ, (void **)&tval_ptr, (void **)&eve_ptr))
         {
-            // if now >= tval_ptr
+            //If now >= tval_ptr
             if(compare((void *)tval_ptr, (void *)&now) <= 0)
             {
-                PQ->removeMin(PQ, (void**)&tval_ptr, (void **)&event_ptr);
-                // add to queue
-                ready_q->enqueue(ready_q, event_ptr);
-                free(tval_ptr);
+                //Remove the minimum event from the priority queue
+                PQ->removeMin(PQ, (void **)&tval_ptr, (void **)&eve_ptr);
+                //Add to queue
+                queue->enqueue(queue, eve_ptr);
+                //TODO: GIVES ERROR WHEN REPEAT CALLED: 
+                //free(tval_ptr);
             }
             else
-            {
                 break;
-            }
-            pthread_mutex_unlock(&lock);
-            
-            // process events in the queue
-            while(ready_q->dequeue(ready_q, (void **)&event_ptr))
+        }
+        pthread_mutex_unlock(&lock);
+        
+        /* Process the events that are in the queue */
+        while(queue->dequeue(queue, (void **)&eve_ptr))
+        {
+            //If the event is not cancel, fire it
+
+            if(eve_ptr->cancel == 0)
             {
-                // if event not canceled, fire
-                if(event_ptr->cancel == 0)
+                printf("Event fired: %lu|%s|%s|%u\n", eve_ptr->clid, eve_ptr->host, eve_ptr->service, eve_ptr->port);
+                if(eve_ptr->repeat == 1 && eve_ptr->n_repeats > 1)
                 {
-                    fprintf(stdout, "Event fired: %lu|%s|%s|%u\n", event_ptr->clid, event_ptr->host, event_ptr->service, event_ptr->port);
-                    if(event_ptr->repeat == 1 && event_ptr->num_repeats > 1)
-                    {
-                        event_ptr->num_repeats--;
-                        repeat_q->enqueue(repeat_q, (void *)event_ptr);
-                    }
-                    else
-                    {
-                        map->remove(map, (void *)&event_ptr->id);
-                        free(tval_ptr);
-                        free(event_ptr->host);
-                        free(event_ptr->service);
-                        free(event_ptr);
-                    }
+                    eve_ptr->n_repeats--;
+                    r_queue->enqueue(r_queue, (void *)eve_ptr);
                 }
-                else 
+                else
                 {
-                    map->remove(map, (void *)&event_ptr->id);
+                    map->remove(map, (void *)&eve_ptr->id);
                     free(tval_ptr);
-                    free(event_ptr->host);
-                    free(event_ptr->service);
-                    free(event_ptr);
-                }
+                    free(eve_ptr->host);
+                    free(eve_ptr->service);
+                    free(eve_ptr);
+                }            
+                
+            }
+            //If it is cancel, recycle the heap storage
+            else{
+                map->remove(map, (void *)&eve_ptr->id);
+                free(tval_ptr);
+                free(eve_ptr->host);
+                /ree(eve_ptr->service);
+                free(eve_ptr);
             }
         }
+
+        /* Go through r_queue and insert the repeat events
+           to the priority queue with a new priority */
         pthread_mutex_lock(&lock);
-        while(repeat_q->dequeue(repeat_q, (void**)&event_ptr))
+        while(r_queue->dequeue(r_queue, (void **)&eve_ptr))
         {
+            //later = now + interval
             later->tv_sec = now.tv_sec;
-            later->tv_usec = now.tv_usec;
+            later->tv_usec = now.tv_usec + eve_ptr->interval*1000;
             while(later->tv_usec > 1000000)
             {
                 later->tv_usec = later->tv_usec - 1000000;
                 later->tv_sec++;
             }
-            if(event_ptr->cancel == 0)
-            {
-                PQ->insert(PQ, (void *)later, (void *)event_ptr);
-            }
-
+            //Insert back into the pq with later as a priority
+            if(eve_ptr->cancel == 0)
+                PQ->insert(PQ, (void *)later, (void *)eve_ptr);
         }
         pthread_mutex_unlock(&lock);
-        ready_q->destroy(ready_q);
-        repeat_q->destroy(repeat_q);
+        queue->destroy(queue);
+        r_queue->destroy(r_queue);
     }
     free(later);
     pthread_exit(NULL);
 }
 
-
-int main(UNUSED int argc, UNUSED char *argv[])
+int main(UNUSED int argc, UNUSED char *argv[]) 
 {
     PQ = HeapPrioQueue(compare, NULL, NULL);
-    if(PQ == NULL)
+    if(PQ == NULL) 
     {
-        fprintf(stderr, "Failure to create Priority Queue\n");
+        fprintf(stderr, "ERROR: Piority Queue creation failed.\n");
     }
-    map = HashMap(128, 4.0, hashFxn, compare_map, NULL, NULL);
-    if(map == NULL)
+    map = HashMap(128, 4.0, hash_fxn, compare_map, NULL, NULL);
+    if(map == NULL) 
     {
-        fprintf(stderr, "Failure to create Map\n");
-
+        fprintf(stderr, "ERROR: HashMap creation failed.\n");
     }
-    pthread_t receiver_thread, timer_thread;
+    
+    pthread_t receiver_t, timer_t;
     pthread_mutex_init(&lock, NULL);
+    
+    pthread_create(&timer_t, NULL, timer, NULL);
+    pthread_create(&receiver_t, NULL, dtsv3, NULL);
+    
+    pthread_join(receiver_t, NULL);
+    pthread_join(timer_t, NULL);
 
-    pthread_create(&timer_thread, NULL, timer, NULL);
-    pthread_create(&receiver_thread, NULL, dtsv3, NULL);
-
-    pthread_join(receiver_thread, NULL);
-    pthread_join(timer_thread, NULL);
     PQ->destroy(PQ);
     map->destroy(map);
+
     return 0;
 }
